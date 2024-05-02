86d4cabc4112b7bdb86e704c01e8db8e7e8e9cef
DaylanDStoica <daylanstoica@gmail.com>
2024-05-02 11:56:43 -0700

    made the sql parsing file

---
+++ sql_txt_parsing.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+// parse the sql database into the txt files for reading
+int sql_to_txt(){
+	return 0;
+
+}
+
+//parse the txt files into database for storage
+int txt_to_sql(){
+	return 0;
+}
