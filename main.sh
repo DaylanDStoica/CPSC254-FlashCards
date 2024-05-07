a99f76f92e494bd2ba55e1ef2346345b61066c98
DaylanDStoica <daylanstoica@gmail.com>
2024-05-07 09:14:50 -0700

    removed execution permissions until ready for effective deployment

--- main.sh
+++ main.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+FILE_PATH = "./LoadingGui/loadFlashCards.sh"
 # main driver shell script for the project
-javac Copy\ of\ GUI/FlashCards.java
+#javac LoadingGUI/FlashCards.java
 # compile the java files for opening GUI for flash card creation
 
-java Copy\ of\ GUI/FlashCards.java 
+#java LoadingGUI/FlashCards.java 
 
 # TODO: create connection to allow viewing of FlashCards as flashcards upon closing of flashcard creation window
+
+#javac LoadingGUI/FlashCardPlayer.java
+#java LoadingGUI/FlashCardPlayer.java
+
+# ./LoadingGui/loadFlashCards.sh
+#sh ./LoadingGui/loadFlashCards.sh
+
+source "$FILE_PATH"
