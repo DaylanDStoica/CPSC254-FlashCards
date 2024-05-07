80efaed5e36a7f41d91a87f24c0a2711c39c91aa
DaylanDStoica <daylanstoica@gmail.com>
2024-05-07 00:39:48 -0700

    created main.sh. So far only compiles and runs the FlashCard creation tool GUI

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# main driver shell script for the project
+javac Copy\ of\ GUI/FlashCards.java
+# compile the java files for opening GUI for flash card creation
+
+java Copy\ of\ GUI/FlashCards.java 
+
+# TODO: create connection to allow viewing of FlashCards as flashcards upon closing of flashcard creation window
